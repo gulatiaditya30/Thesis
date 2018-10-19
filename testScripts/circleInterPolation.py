4c68b0fd6b51621decbfed653b9462e0c53b870d
aditya <gulatiaditya30@gmail.com>
2018-10-19 11:52:17 +0200

    circle interpolation working fine

--- testScripts/circleInterPolation.py
+++ testScripts/circleInterPolation.py
@@ -28,19 +28,22 @@ def getPoint():
     robot.close()
     #return [currentState.pos.x,currentState.pos.y,currentState.pos.z]
 
-def drawCircle(x1,y1,x2,y2,x3,y3):
+def moveAroundCircle(x1,y1,x2,y2,x3,y3):
     robot  = urx.Robot("160.69.69.101")
     currentState = robot.get_pose()
     i=0
     circleInfo = circlePolation(x1,y1,x2,y2,x3,y3)
     circumPoints = circumfenceCoordinate(circleInfo[0],circleInfo[1],circleInfo[2])
-    print (circumPoints[10].x)
+    print("@@@@@@@@@@@@@@@@@@@@@@@@*********************************@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print (len(circumPoints))
     
-    while(i<361):
-        currentState.pos.x = circumPoints[i].x
-        currentState.pos.y = circumPoints[i].y
+    while(i<360):
+        currentState = robot.get_pose()
+        currentState.pos.x = circumPoints[0][i]
+        currentState.pos.y = circumPoints[1][i]
+        print(str(circumPoints[0][i]) +","+str(circumPoints[1][i]))
         robot.set_pose(currentState,vel = 0.050,acc =2 )
-        i=+1
+        i=i+1
 
     robot.close()
 
@@ -50,7 +53,7 @@ def circlePolation(x1,y1,x2,y2,x3,y3):
 
     a =  x1 * (y2 - y3) - y1 * (x2 - x3) + x2 * y3 - x3 * y2
 
-    b = (x1 * x1 + y1 * y1) * (y3 - y2) + (x2 * x2 + y2 * y2) * (y1 - y3) + (x3 * x3 + y3 * y3) * (x1 - x2)
+    b = (x1 * x1 + y1 * y1) * (y3 - y2) + (x2 * x2 + y2 * y2) * (y1 - y3) + (x3 * x3 + y3 * y3) * (y2 - y1)
 
     c = (x1 * x1 + y1 * y1) * (x2 - x3) + (x2 * x2 + y2 * y2) * (x3 - x1) + (x3 * x3 + y3 * y3) * (x1 - x2)
 
@@ -67,15 +70,17 @@ def circumfenceCoordinate(Cx,Cy,r):
     xArr = []
     yArr = []
 
-    while (i<361):
+    while (i<95):
         
 
-        x = ((r* math.cos(i)) + Cx)*0.001
-        y = ((r*math.sin(i)) + Cy)*0.001
+        x = ((r* math.cos(i)) + Cx)#*0.001
+        y = ((r*math.sin(i)) + Cy)#*0.001
+        xArr.append(x)
+        yArr.append(y)
         p = coordinate(x,y)
         circumCoordinates.append(p)
         i+=1
-    return circumCoordinates
+    return [xArr,yArr]
 
 if __name__ == "__main__":
 
@@ -110,21 +115,33 @@ if __name__ == "__main__":
         if (chr(keyInput & 255) == "q"):
             exit()
         elif(chr(keyInput &255)=="p"):
-            xArr = []
-            yArr = []
-            i=0
-            z = circlePolation(initialPoints[0],initialPoints[1],initialPoints[2],initialPoints[3],initialPoints[4],initialPoints[5])
-            o = circumfenceCoordinate(z[0],z[1],z[2])
-            while(i<360):
-                xArr.append(o[i].x)
-                yArr.append(o[i].y)
-                i+=1
-            plt.plot(xArr,yArr,'ro')
-            plt.show()
+            print(len(initialPoints))
+            if (len(initialPoints)<6):
+                print("select atleast 3 points on the circumfence")
+            else:
+                xArr = []
+                yArr = []
+                i=0
+                z = circlePolation(initialPoints[0],initialPoints[1],initialPoints[2],initialPoints[3],initialPoints[4],initialPoints[5])
+                o = circumfenceCoordinate(z[0],z[1],z[2])
+                while(i<90):
+                    xArr.append(o[0][i])
+                    yArr.append(o[1][i])
+                    i+=1
+                plt.plot(xArr,yArr,'ro')
+                iniX = [z[0],initialPoints[0],initialPoints[2],initialPoints[4]]
+                iniY = [z[1],initialPoints[1],initialPoints[3],initialPoints[5]]
+                plt.plot(iniX,iniY,'go')
+                plt.show()
 
         elif (chr(keyInput & 255) == "k"):
             _thread.start_new_thread(getPoint, ())
+
+
         elif(chr(keyInput & 255 )=="d"):
-            _thread.start_new_thread(drawCircle,(initialPoints[0],initialPoints[1],initialPoints[2],initialPoints[3],initialPoints[4],initialPoints[5])) 
+            if (len(initialPoints)<6):
+                print("select atleast 3 points on the circumfence")
+            else:
+                _thread.start_new_thread(moveAroundCircle,(initialPoints[0],initialPoints[1],initialPoints[2],initialPoints[3],initialPoints[4],initialPoints[5])) 
 
 
