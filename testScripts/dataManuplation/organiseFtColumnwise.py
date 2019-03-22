14388b13f4f4ee439cf092a36d2b84f5d16f0bc0
aditya <gulatiaditya30@gmail.com>
2019-03-22 16:23:42 +0100

    finally data arranged columnwise in 1 file

--- testScripts/dataManuplation/organiseFtColumnwise.py
+++ testScripts/dataManuplation/organiseFtColumnwise.py
@@ -3,8 +3,7 @@ import numpy as np
 from sklearn import preprocessing
 import os
 
-
-
+'''
 finalGood=[]
 finalBad=[]
 
@@ -25,9 +24,9 @@ for plateNo in range(0,9,1):
                     continue
                 if(labelsValue[int(k[0])][6]=="TRUE"):
                     goodC=goodC+1
-                    finalGood.append(['C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/ft_plate_'+str(plateNo)+'/normalised/'+str(f)])
+                    finalGood.append(['C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/ft_plate_'+str(plateNo)+'/newNormalised/'+str(f)])
                 elif(labelsValue[int(k[0])][6]=="FALSE"):
-                    finalBad.append(["C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/ft_plate_"+str(plateNo)+"/normalised/"+str(f)])
+                    finalBad.append(["C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/ft_plate_"+str(plateNo)+"/newNormalised/"+str(f)])
             
         print(" plate no : "+str(plateNo)+ " done =============")
 
@@ -45,58 +44,50 @@ with open("C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/columnwis
         writer.writerow(line)
 goodWrite.close() 
 badWrite.close()   
-    
-    
-
 '''
-with open("address.csv") as ftFile:
 
-    fileReader = csv.reader(ftFile,delimiter=",")
-
-    for row in fileReader:
-        fx = row[0]
-
-for j in range(8,9,1):  # the plate 
+columnft = []
+with open("C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/columnwise/bad/badAdress.csv") as badAdress:
+    badAddressReader = csv.reader(badAdress,delimiter = ",")
     
-    for i in range(0,280,1): # the rivet no
-        if(i in [120,121,160,161,200,201,240,241]): #missing rivets in 0-4 plates [120,159,160,199,200,239,240,279]
-             continue
-        z = np.loadtxt(open("C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/ft_plate_"+str(j)+"/forcelog"+str(i) +".csv", "rb"), delimiter=",")
+    i = 0
+    for row in badAddressReader:
+        rivetcolumnft = []
+        with open(str(row[0])) as tempRivetCSV:
+            rivetcolumnft.append(0)
+            tempRivetReader = csv.reader(tempRivetCSV,delimiter=",")
+            for k in tempRivetReader:
+                rivetcolumnft.append(k[9])
+        columnft.append(rivetcolumnft)
+        #print("rivet"+str(i) +"done")
+        i=i+1
 
-        #print("The problem rivet :"+ str(i))
+with open("C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/columnwise/good/goodAdress.csv") as goodAdress:
+    goodAddressReader = csv.reader(goodAdress,delimiter = ",")
+    i = 0
+    for row in goodAddressReader:
+        rivetcolumnft = []
+        with open(str(row[0])) as tempRivetCSV:
+            rivetcolumnft.append(1)
+            tempRivetReader = csv.reader(tempRivetCSV,delimiter=",")
+            for k in tempRivetReader:
+                rivetcolumnft.append(k[9])
+        columnft.append(rivetcolumnft)
+        #print("rivet"+str(i) +"done")
+        i=i+1
 
-        fx = z[:,0]
-        fy = z[:,1]
-        fz = z[:,2]
 
-        
-        fx = preprocessing.normalize([fx])
-        fxN = fx[0,:]
 
-        fy = preprocessing.normalize([fy])
-        fyN = fy[0,:]
 
-        fz = preprocessing.normalize([fz])
-        fzN = fz[0,:]
-        
-        del z
 
-        with open('C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/ft_plate_'+str(j)+'/normalised/forcelogN'+str(i)+'.csv','a',newline='') as fileOut:
-            writer = csv.writer(fileOut)
-            with open("C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/ft_plate_"+str(j)+"/forcelog"+str(i)+".csv") as ftFile:        
-                c=0
-                fileReader = csv.reader(ftFile,delimiter=",")
-                for row in fileReader:    
-                    row.append(fxN[c])
-                    row.append(fyN[c])
-                    row.append(fzN[c])
-                    writer.writerow(row)
-                    c=c+1
-                    if(c==499):
-                        break
-        
-        ftFile.close()
-        fileOut.close()
-    print("++++++++++++++++++++++"+str(j)+"Plate done ++++++++++++++++++++++++++++++++++")'''
 
-        
+    with open("C:/Users/gulat/Desktop/thesis/gitThesis/trainingData/ftData/columnwise/ftDataColumwise.csv","w",newline='') as columnFtFile:
+        print("========================== WRITING INTO COLUMN FILE ============================")
+        for line in columnft:
+            writer= csv.writer(columnFtFile)
+            writer.writerow(line)
+
+    
+
+
+
